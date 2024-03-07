4e84d7cf7a30b364547cc7813cdd6e8ed1ccdf70
wwt1983 <wwt1983@yandex.ru>
2024-03-07 19:54:05 +0500

    test routing + add pages home about

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react";
+import {
+  createBrowserRouter,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+import About from "./pages/about";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Тепло
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Link className="site-logo" to="/">
+            #LOGO
+          </Link>
+          <nav>ß
+            <Link to="/about">О компании</Link>
+          </nav>
+        </header>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
